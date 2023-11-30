98b83e5fb32e8d59c7614f58113e9abc45a213f4
“leozam02” <ltzambra@espol.edu.ec>
2023-11-29 19:28:14 -0500

    Primer commit

---
+++ src/main/java/ec/edu/espol/juego/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.juego;
+
+/**
+ *
+ * @author leoza
+ */
+public class Juego {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
