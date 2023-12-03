a34e76d0a173472b5123644eaefd683c6ab8ea0f
PoliJaren1015 <144569819+PoliJaren1015@users.noreply.github.com>
2023-12-03 09:42:26 -0500

    Update Juego.java

--- src/main/java/ec/edu/espol/juego/Juego.java
+++ src/main/java/ec/edu/espol/juego/Juego.java
@@ -45,5 +45,66 @@ public class Juego {
         System.out.println();
     }
 
+    public boolean agregarFichaLinea(Ficha ficha, Jugador jugador) {
+    if (!(ficha instanceof FichaComodin)) {
+        if (lineaJuego.isEmpty()) {
+            lineaJuego.add(ficha);
+            
+            
+            return true;
+        } else {
+            int valorInicio = this.obtenerValorInicioLinea();
+            int valorFin = this.obtenerValorFinLinea();
+            if (ficha.getLado2() == valorInicio) {
+                lineaJuego.add(0, ficha);
+                return true;
+                
+            } else if (ficha.getLado1() == valorFin) {
+                lineaJuego.add(ficha);
+                
+                return true;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        if (lineaJuego.isEmpty()) {
+            lineaJuego.add(ficha);
+            System.out.print("Ingrese el valor del lado1: ");
+            int lado1 = sc.nextInt();
+            System.out.print("Ingrese el valor del lado2: ");
+            int lado2 = sc.nextInt();
+            FichaComodin f = (FichaComodin) ficha;
+            f.setLado1(lado1);
+            f.setLado2(lado2);
+            return true;
+        } else {
+            System.out.print("Ingrese la posición (Inicio o Fin): ");
+            String posicion = sc.next().toLowerCase();
+            if (posicion.equals("inicio")) {
+                System.out.print("Ingrese el valor del lado1: ");
+                int lado1 = sc.nextInt();
+                FichaComodin f = (FichaComodin) ficha;
+                f.setLado1(lado1);
+                lineaJuego.add(0, f);
+                
+                return true;
+            } else if (posicion.equals("fin")) {
+                System.out.print("Ingrese el valor del lado2: ");
+                int lado2 = sc.nextInt();
+                FichaComodin f = (FichaComodin) ficha;
+                f.setLado2(lado2);
+                lineaJuego.add(f);
+                
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    }   
+
+    // Colocar Juego Maquina, jugar y main
+
   
 }
