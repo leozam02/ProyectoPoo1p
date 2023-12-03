a508f874bf487c8d20372b46d930629cc6a21b44
Brun025-5 <152760572+Brun025-5@users.noreply.github.com>
2023-12-02 23:20:05 -0500

    Update Ficha.java
    Cambio de paquete para correcta ejecución

--- src/main/java/ec/edu/espol/juego/Ficha.java
+++ src/main/java/ec/edu/espol/juego/Ficha.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ec.edu.espol.proyecto1pmio;
+package ec.edu.espol.juego;
 
 /**
  *
@@ -24,7 +24,6 @@ public class Ficha {
     public int getLado2() {
         return lado2;
     }
-    
     
     @Override
     public String toString(){
